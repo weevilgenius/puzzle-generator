a6935e17b86b9cabd3e8bd9200ee2c9c8a19ffc8
Barry Stump <weevilgenius@users.noreply.github.com>
2025-06-24 10:25:09 -0700

    add Mithril

--- src/index.ts
+++ src/index.ts
@@ -1,79 +1,59 @@
-import { Delaunay } from 'd3-delaunay';
-import PoissonDiskSampling from 'poisson-disk-sampling';
+import m from 'mithril';
+import { generateVoronoi, renderToCanvas } from "./shared/geometry";
 
 // include our CSS
 import './index.css';
 
+// page component
+const Page: m.ClosureComponent<unknown> = () => {
 
-// control values
-const W = 800, H = 600;
-const dist = 40;
+  // component state
+  const state = {
+    /** Width of canvas in pixels */
+    canvasWidth: 800,
+    /** Height of canvas in pixels */
+    canvasHeight: 600,
+    /** Minimum distance between control points (pixels) */
+    distance: 40,
+    /** Canvas HTML element */
+    canvas: null as HTMLCanvasElement | null,
+  };
 
-// generate points, distribute them
-const poisson = new PoissonDiskSampling({
-  shape: [W, H],
-  minDistance: dist,
-  tries: 20,
-});
-const pts = poisson.fill() as [number, number][];
-console.log(`generated ${pts.length} points`);
+  function rebuildVoronoi() {
+    if (!state.canvas) return;
+    const { voronoi } = generateVoronoi(state.canvasWidth, state.canvasHeight, state.distance);
+    renderToCanvas(voronoi, state.canvas);
+  }
 
-// build voronoi
-const delaunay = Delaunay.from(pts);
-const voronoi = delaunay.voronoi([0, 0, W, H]); // extent = rectangle
-// (cells are already clipped to the rectangle)
+  // Mithril component
+  return {
+
+    // component lifecycle: called after our DOM element is created and attached
+    oncreate: ({ dom }) => {
+      state.canvas = dom.querySelector<HTMLCanvasElement>("canvas");
+      rebuildVoronoi();
+    },
+
+    // component lifecycle: render our output
+    view: () => {
+
+      return m(".container", [
+        m('canvas', {
+          width: state.canvasWidth,
+          height: state.canvasHeight,
+        }),
+        m("button.rebuild", {
+          onclick: () => {
+            rebuildVoronoi();
+          },
+        }, "Rebuild"),
+      ]);
+    }, // view()
+  };
+};
 
 
-// draw on canvas
-const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-if (!canvas) throw new Error("No canvas found");
-canvas.setAttribute("width", W.toString());
-canvas.setAttribute("height", H.toString());
-const ctx = canvas.getContext("2d");
-
-if (ctx) {
-  // draw the background
-  ctx.fillStyle = "#ffffff33";
-  ctx.fillRect(0, 0, W, H);
-
-  // draw the voronoi polygons and borders
-  ctx.fillStyle = "black";
-  ctx.beginPath();
-  voronoi.update().render(ctx);
-  voronoi.renderBounds(ctx);
-  ctx.stroke();
-
-  // draw the control points
-  ctx.beginPath();
-  voronoi.delaunay.renderPoints(ctx, 2);
-  ctx.fill();
-} else {
-  throw new Error("No 2D context");
-}
-
-/*
-// convert to SVG
-const svg = document.getElementById("image");
-if (!svg) throw new Error("No SVG found");
-
-// set the size
-svg.setAttribute("width", W.toString());
-svg.setAttribute("height", H.toString());
-
-// draw points
-pts.forEach(([x, y]) => {
-  const c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-  c.setAttribute("cx", x.toString()); c.setAttribute("cy", y.toString()); c.setAttribute("r", "3");
-  c.setAttribute("class", "point");
-  svg.appendChild(c);
-});
-
-// draw cells
-pts.forEach((_, i) => {
-  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  path.setAttribute("d", voronoi.renderCell(i));   // renderCell returns an SVG path string
-  path.setAttribute("class", "cell");
-  svg.appendChild(path);
-});
-*/
-
+// Ask Mithril to render the page, our componet gets placed into the root element.
+// Mithril will rerender automatically after DOM event handlers defined in component
+// views and also whenever m.redraw() is called.
+m.mount(document.body, Page);
---
+++ src/shared/geometry.ts
@@ -0,0 +1,70 @@
+import { Delaunay, type Voronoi } from 'd3-delaunay';
+import PoissonDiskSampling from 'poisson-disk-sampling';
+export type { Voronoi } from 'd3-delaunay';
+
+export type Vec2 = [number, number];
+
+// utility to generate random points and the Voronoi diagram from them
+export function generateVoronoi(width: number, height: number, distance: number): { points: Vec2[], voronoi: Voronoi<Vec2> } {
+  // generate points randomly in a Poisson disk sampling
+  const poisson = new PoissonDiskSampling({
+    shape: [width, height],
+    minDistance: distance,
+    tries: 20,
+  });
+  const points = poisson.fill() as Vec2[];
+  console.log(`generated ${points.length} points`);
+
+  // build voronoi diagram
+  const delaunay = Delaunay.from(points) as Delaunay<Vec2>;
+  const voronoi = delaunay.voronoi([0, 0, width, height]);
+  // cells are automatically clipped to the boundary rectangle
+
+  return { points, voronoi };
+}
+
+// utility to render the voronoi and control points to the canvas
+export function renderToCanvas(voronoi: Voronoi<Vec2>, canvas: HTMLCanvasElement) {
+  if (!canvas) throw new Error("No canvas found");
+  const ctx = canvas.getContext("2d")!;
+
+  // draw the background
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = '#ffffff';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // draw the voronoi cells and bounds
+  ctx.beginPath();
+  voronoi.render(ctx);
+  voronoi.renderBounds(ctx);
+  ctx.strokeStyle = '#333';
+  ctx.lineWidth = 1;
+  ctx.stroke();
+
+  // draw the control points
+  ctx.beginPath();
+  voronoi.delaunay.renderPoints(ctx, 2);
+  ctx.fillStyle = "crimson";
+  ctx.fill();
+}
+
+// utility to render the voronoi and control points to SVG
+export function renderToSVG(voronoi: Voronoi<Vec2>, svg: SVGElement) {
+  if (!svg) throw new Error("No SVG found");
+
+  // draw points
+  const points = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  points.setAttribute("d", voronoi.delaunay.renderPoints());
+  svg.appendChild(points);
+
+  // draw cells
+  const cells = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  cells.setAttribute("d", voronoi.render());
+  svg.appendChild(cells);
+
+  // draw bounds
+  const bounds = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  bounds.setAttribute("d", voronoi.renderBounds());
+  svg.appendChild(bounds);
+}
+
