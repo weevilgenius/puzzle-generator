0f058d41901d2541b82b08587dcf5b175c35cf44
Barry Stump <weevilgenius@users.noreply.github.com>
2026-02-20 11:35:14 -0800

    deps: bump rollup-plugin-visualizer from 6.0.5 to 7.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -11,11 +11,12 @@ export default defineConfig(() => {
     // add the visualizer to see the module break-down
     plugins.push(
       visualizer({
-        open: true,
         filename: 'dist/stats.html',
+        template: 'treemap', // Report type: sunburst, treemap, network, raw-data, list, markdown, flamegraph
+        open: true,
         gzipSize: true,
         brotliSize: true,
-      })
+      }) as PluginOption
     );
   }
 
